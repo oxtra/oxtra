afa4debcf224a7de3164d1814f2d6cba981388f7
bjoernbossh <bjoernboss.henrichsen@tum.de>
2019-06-01 14:56:25 +0200

    Adding of namespaces

--- src/oxtra/codegen/codestore/codestore.h
+++ src/oxtra/codegen/codestore/codestore.h
@@ -1,4 +1,10 @@
 #ifndef OXTRA_CODESTORE_H
 #define OXTRA_CODESTORE_H
 
+namespace codegen {
+	namespace codestore {
+
+	}
+}
+
 #endif //OXTRA_CODESTORE_H
--- src/oxtra/dispatcher/dispatcher.h
+++ src/oxtra/dispatcher/dispatcher.h
@@ -1,4 +1,8 @@
 #ifndef OXTRA_DISPATCHER_H
 #define OXTRA_DISPATCHER_H
 
+namespace dispatcher {
+
+}
+
 #endif //OXTRA_DISPATCHER_H
