bd3ea5b9a43c81a9a8a10abbc6477074b12e6a39
Knud Haase <knud.haase@tum.de>
2019-06-29 10:18:27 +0200

    Second try

--- test/oxtra/encoding/encoding_test.cpp
+++ test/oxtra/encoding/encoding_test.cpp
@@ -7,22 +7,23 @@ using namespace encoding;
 TEST_CASE("instruction encoding is correct", "[encoding]") {
 	SECTION("LUI") {
 		FILE* assembly = fopen("lui.s", "w");
-		fprintf(assembly, "lui t4, 2874");
+		fprintf(assembly, "lui t4, 28");
 		fclose(assembly);
 
-		utils::riscv_instruction_t lui_encoded = LUI(RiscVRegister::t4, 2874);
+		utils::riscv_instruction_t lui_encoded_ = LUI(RiscVRegister::t4, 28);
+		char* lui_encoded = reinterpret_cast<char*>(&lui_encoded_);
 
 		system("/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc lui.s -o lui -nostdlib");
 		system("objcopy -I elf64-little -j .text -O binary lui luidump");
 
-		char lui_compiled[5];
+		char lui_compiled[5] = {0};
 		FILE* objdump = fopen("luidump", "r");
-		fgets(lui_compiled, 4, objdump);
+		fread(lui_compiled, 1, 4, objdump);
 		fclose(objdump);
 
-		REQUIRE(lui_compiled[0] == (char)(lui_encoded >> 24));
-		REQUIRE(lui_compiled[1] == (char)(lui_encoded >> 16));
-		REQUIRE(lui_compiled[2] == (char)(lui_encoded >> 8));
-		REQUIRE(lui_compiled[3] == (char)(lui_encoded));
+		REQUIRE(lui_compiled[0] == lui_encoded[0]);
+		REQUIRE(lui_compiled[1] == lui_encoded[1]);
+		REQUIRE(lui_compiled[2] == lui_encoded[2]);
+		REQUIRE(lui_compiled[3] == lui_encoded[3]);
 	}
 }
