40b329b24d8b2f87295735deeb872063b7532975
bjoernboss <bjoernboss.henrichsen@tum.de>
2019-06-29 19:51:28 +0200

    Add memory-operand translation

--- src/oxtra/codegen/codegen.cpp
+++ src/oxtra/codegen/codegen.cpp
@@ -1,4 +1,3 @@
-#include <oxtra/codegen/encoding/encoding.h>
 #include "oxtra/codegen/codegen.h"
 #include <spdlog/spdlog.h>
 
@@ -7,6 +6,7 @@ using namespace utils;
 using namespace codestore;
 using namespace fadec;
 using namespace encoding;
+
 
 CodeGenerator::CodeGenerator(const arguments::Arguments& args, const elf::Elf& elf)
 		: _args{args}, _elf{elf}, _codestore{args, elf} {}
@@ -57,8 +57,7 @@ host_addr_t CodeGenerator::translate(guest_addr_t addr) {
 		current_address += x86_instruction.get_size();
 		addr += x86_instruction.get_size();
 
-		if constexpr (SPDLOG_ACTIVE_LEVEL <= SPDLOG_LEVEL_TRACE)
-		{
+		if constexpr (SPDLOG_ACTIVE_LEVEL <= SPDLOG_LEVEL_TRACE) {
 			char formatted_string[512];
 			fadec::format(x86_instruction, formatted_string, sizeof(formatted_string));
 
@@ -77,7 +76,7 @@ host_addr_t CodeGenerator::translate(guest_addr_t addr) {
 }
 
 bool CodeGenerator::translate_instruction(const fadec::Instruction& x86_instruction,
-		utils::riscv_instruction_t* riscv_instructions, size_t& num_instructions) {
+										  utils::riscv_instruction_t* riscv_instructions, size_t& num_instructions) {
 	switch (x86_instruction.get_type()) {
 		// at the moment we just insert a return for every instruction that modifies control flow.
 		case InstructionType::JMP:
@@ -106,12 +105,55 @@ bool CodeGenerator::translate_instruction(const fadec::Instruction& x86_instruct
 	return false;
 }
 
-size_t CodeGenerator::translate_mov(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction) {
+
+void CodeGenerator::translate_memory_operand(const fadec::Instruction& x86_instruction,
+											 utils::riscv_instruction_t* riscv_instructions, size_t& num_instructions,
+											 size_t index, RiscVRegister reg) {
+	if (x86_instruction.get_address_size() != 8)
+		throw std::runtime_error("invalid addressing-size");
+
+	const auto& operand = x86_instruction.get_operand(index);
+
+	// add the scale & index
+	if (x86_instruction.get_index_register() != fadec::Register::none) {
+		riscv_instructions[num_instructions++] = encoding::MV(reg, register_mapping[static_cast<uint16_t>(
+				x86_instruction.get_index_register())]);
+		riscv_instructions[num_instructions++] = encoding::SLLI(reg, reg, x86_instruction.get_index_scale());
+	}
+
+	// add the base-register
+	if (operand.get_register() != fadec::Register::none) {
+		riscv_instructions[num_instructions++] = encoding::ADD(reg, reg, register_mapping[static_cast<uint16_t>(
+				operand.get_register())]);
+	}
+
+	// add the displacement
+	if (x86_instruction.get_displacement() > 0) {
+		// less or equal than 12 bits
+		if (x86_instruction.get_displacement() < 0x1000) {
+			riscv_instructions[num_instructions++] = encoding::ADDI(reg, reg, static_cast<uint16_t>(
+					x86_instruction.get_displacement()));
+		} else {
+			riscv_instructions[num_instructions++] = encoding::LUI(RiscVRegister::t6,
+																   static_cast<uint32_t>(x86_instruction.get_displacement())
+																		   >> 12u);
+			riscv_instructions[num_instructions++] = encoding::ADDI(RiscVRegister::t6, RiscVRegister::t6,
+																	static_cast<uint16_t>(x86_instruction.get_displacement()) &
+																	0x0FFFu);
+			riscv_instructions[num_instructions++] = encoding::ADD(reg, reg, RiscVRegister::t6);
+		}
+	}
+}
+
+size_t
+CodeGenerator::translate_mov(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction) {
+
 	riscv_instruction[0] = LUI(RiscVRegister::a0, static_cast<uint16_t>(x86_instruction.get_immediate()));
 	return 1;
 }
 
-size_t CodeGenerator::translate_ret(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction) {
+size_t
+CodeGenerator::translate_ret(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction) {
 	riscv_instruction[0] = JALR(RiscVRegister::zero, RiscVRegister::ra, 0);
 	return 1;
 }
--- src/oxtra/codegen/codegen.h
+++ src/oxtra/codegen/codegen.h
@@ -5,10 +5,31 @@
 #include "oxtra/arguments/arguments.h"
 #include "oxtra/codegen/codestore/codestore.h"
 #include "oxtra/elf/elf.h"
+#include "oxtra/codegen/encoding/encoding.h"
 #include <fadec.h>
 
 namespace codegen {
 	class CodeGenerator {
+	private:
+		static constexpr encoding::RiscVRegister register_mapping[] = {
+			encoding::RiscVRegister::a0, //rax
+			encoding::RiscVRegister::a2, //rcx
+			encoding::RiscVRegister::a3, //rdx
+			encoding::RiscVRegister::a1, //rbx
+			encoding::RiscVRegister::sp, //rsp
+			encoding::RiscVRegister::s0, //rbp
+			encoding::RiscVRegister::a4, //rsi
+			encoding::RiscVRegister::a5, //rdi
+			encoding::RiscVRegister::a6, //r8
+			encoding::RiscVRegister::a7, //r9
+			encoding::RiscVRegister::s2, //r10
+			encoding::RiscVRegister::s3, //r11
+			encoding::RiscVRegister::s4, //r12
+			encoding::RiscVRegister::s5, //r13
+			encoding::RiscVRegister::s6, //r14
+			encoding::RiscVRegister::s7  //r15
+		};
+
 	private:
 		const arguments::Arguments& _args;
 		const elf::Elf& _elf;
@@ -24,8 +45,6 @@ namespace codegen {
 	public:
 		utils::host_addr_t translate(utils::guest_addr_t addr);
 
-		const arguments::Arguments& get_args() const;
-
 	private:
 		/**
 		 * Translates a x86 instruction into multiple risc-v instructions.
@@ -34,9 +53,25 @@ namespace codegen {
 		 * @param num_instructions Reference to the number of instructions that were written to the array.
 		 * @return Returns whether the this instruction ends the basic block.
 		 */
-		bool translate_instruction(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instructions, size_t& num_instructions);
+		bool
+		translate_instruction(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instructions,
+							  size_t& num_instructions);
+
+		/**
+		 * Translates a x86-memory operand into risc-v instructions (resulting address in reg)
+		 * t6 might be overridden.
+		 * @param x86_instruction The x86 instruction object.
+		 * @param riscv_instructions An array of risc-v instructions.
+		 * @param num_instructions current number of risc-v instructions.
+		 * @param index operand-index of instruction.
+		 * @param reg The resulting address will be returned in this register. (t6 may not be used)
+		 */
+		static void translate_memory_operand(const fadec::Instruction& x86_instruction,
+											 utils::riscv_instruction_t* riscv_instructions, size_t& num_instructions,
+											 size_t index, encoding::RiscVRegister reg);
 
 		size_t translate_mov(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction);
+
 		size_t translate_ret(const fadec::Instruction& x86_instruction, utils::riscv_instruction_t* riscv_instruction);
 	};
 }
