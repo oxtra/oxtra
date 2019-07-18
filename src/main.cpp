958eaca5a7fbb047ff554a4d86fd4970f06d9096
bjoernboss <bjoernboss.henrichsen@tum.de>
2019-07-18 23:43:02 +0200

    Implement initial idea for new logging

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@ int main(int argc, char** argv) {
 	//parse the arguments
 	const auto arguments = arguments::Arguments(argc, argv);
 	spdlog::set_level(arguments.get_log_level());
-	SPDLOG_INFO("Finished parsing the arguments.");
+	spdlog::info("Finished parsing the arguments.");
 
 	//create the elf-object
 	const auto elf = elf::Elf(arguments.get_guest_path());
-	SPDLOG_INFO("Finished reading and parsing elf file.");
+	spdlog::info("Finished reading and parsing elf file.");
 
 	//create the dispatcher
 	dispatcher::Dispatcher dispatcher(elf, arguments);
-	SPDLOG_INFO("Finished creating and initializing various runtime-objects.");
+	spdlog::info("Finished creating and initializing various runtime-objects.");
 
 	//startup the translation and execution of the source-code
 	const auto return_value = dispatcher.run();
