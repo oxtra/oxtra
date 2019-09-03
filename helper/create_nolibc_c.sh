fcad4bc41275218513da453f0973b296b5b121ee
Michael Plainer <plainer@ymail.com>
2019-09-03 14:57:14 +0200

    Add predefined helper methods to script

--- helper/create_nolibc_c.sh
+++ helper/create_nolibc_c.sh
@@ -5,42 +5,60 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-syscall=$'int _syscall(int number, int param1) {
-	int ret;
-
-	asm(\"mov eax, %1;\"
-	\"mov edi, %2;\"
-	\"syscall;\"
-	\"mov %0, eax;\"
-	: \"=r\" (ret)
-	: \"r\" (number), \"r\" (param1)
+syscall=$'long _syscall(long number, long param1, long param2, long param3) {
+	long ret;
+	
+	asm("mov rax, %1;"
+	"mov rdi, %2;"
+	"mov rdx, %4;"
+	"mov rsi, %3;"
+	"syscall;"
+	"mov %0, rax;"
+	: "=r" (ret)
+	: "r" (number), "r" (param1), "r" (param2), "r" (param3)
 	);
-						
-	return ret;
+				
+        return ret;
+}\n'
+
+write=$'void _write (int fd, const void *buf, long count) {
+	_syscall(1, fd, buf, count);
+}\n'
+
+print=$'void print(const void *buf) {
+	if (buf == 0) return;
+	int length;
+	for (length = 0; ((char*) buf)[length] != \'\\0\'; length++);
+	_write(1, buf, length);
+}\n'
+
+read=$'long read(int fd, void *buf, long count) {
+	return _syscall(0, fd, buf, count);
 }\n'
 
 echo "Enter your C code and finish with Ctrl+D"
+echo $'The main():int method will be called\n'
 
-echo "$syscall"
+echo "long _syscall(long number, long param1, long param2, long param3);"
+echo "void _write (int fd, const void *buf, long count);"
+echo "void print(const void *buf);"
+echo "long read(int fd, void *buf, long count);"
 
-echo "int main() { "
+echo ""
 
 readarray -t lines
 
-echo "}"
-
 echo "$syscall" > $1.c
-
-echo "int main() { " >> $1.c
+echo "$write" >> $1.c
+echo "$print" >> $1.c
+echo "$read" >> $1.c
 
 for line in "${lines[@]}"; do
-	echo "	$line" >> $1.c
+	echo "$line" >> $1.c
 done
 
-echo $'}\n' >> $1.c
-
 echo "void _start() { " >> $1.c
-echo "	_syscall(60, main());" >> $1.c
+echo "	_syscall(60, main(), 0, 0);" >> $1.c
 echo "}" >> $1.c
 
 gcc -static -nostdlib -m64 -masm=intel $1.c -o $1
