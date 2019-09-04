adb9c7485c90bd6991460d0c4f6c9c64cecb896c
Michael Plainer <plainer@ymail.com>
2019-09-04 22:51:50 +0200

    Fix shift amount in CSEP

--- src/oxtra/codegen/instructions/memory/csep.cpp
+++ src/oxtra/codegen/instructions/memory/csep.cpp
@@ -6,7 +6,7 @@ void codegen::CSep::generate(CodeBatch& batch) const {
 		batch += encoding::SRAI(encoding::RiscVRegister::rdx, encoding::RiscVRegister::rax, 63);
 	} else {
 		// set all bits of t0 to one or zero depending on the sign
-		batch += encoding::SLLI(encoding::RiscVRegister::t0, encoding::RiscVRegister::rax, get_operand_size() * 8);
+		batch += encoding::SLLI(encoding::RiscVRegister::t0, encoding::RiscVRegister::rax, 64 - get_operand_size() * 8);
 		batch += encoding::SRAI(encoding::RiscVRegister::t0, encoding::RiscVRegister::t0, 63);
 
 		codegen::helper::move_to_register(batch, encoding::RiscVRegister::rdx, encoding::RiscVRegister::t0,
