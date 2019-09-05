9f07c4547034cc33e6706ab2a469726d99053d98
Michael Plainer <plainer@ymail.com>
2019-09-05 11:56:37 +0200

    Add basic structure for (I)MUL

--- src/oxtra/codegen/instructions/arithmetic/mul.cpp
+++ src/oxtra/codegen/instructions/arithmetic/mul.cpp
@@ -1,7 +1,67 @@
+#include "oxtra/codegen/helper.h"
 #include "mul.h"
 
+/**
+ * This implementation can handle MUL, IMUL, IMUL2, and IMUL3.
+ * Two source operands (s1 and s2) are multiplied and stored in the correct destination register.
+ */
 void codegen::Mul::generate(codegen::CodeBatch& batch) const {
+	const auto op_size = get_operand(0).get_size();
 
+	const auto is_signed = get_type() != fadec::InstructionType::MUL;
+	const auto has_upper_destination =
+			get_type() == fadec::InstructionType::MUL || get_type() == fadec::InstructionType::IMUL;
+
+	// the default values are for MUL and IMUL
+	encoding::RiscVRegister upper_destination = encoding::RiscVRegister::rdx;
+	encoding::RiscVRegister lower_destination = encoding::RiscVRegister::rax;
+	encoding::RiscVRegister src1 = encoding::RiscVRegister::rax; // t1 reserved for this register
+	encoding::RiscVRegister src2 = helper::map_reg(get_operand(0).get_register()); // t2 reserverd for this register
+
+	if (get_type() == fadec::InstructionType::IMUL2) {
+		lower_destination = helper::map_reg(get_operand(0).get_register());
+		src1 = helper::map_reg(get_operand(0).get_register());
+		src2 = helper::map_reg(get_operand(1).get_register());
+	} else if (get_type() == fadec::InstructionType::IMUL3) {
+		lower_destination = helper::map_reg(get_operand(0).get_register());
+		src1 = encoding::RiscVRegister::t1;
+		helper::load_immediate(batch, get_immediate(), src1);
+	}
+
+	if (op_size == 8) {
+		// if the first multiplication would override a source, we have to use a temporary register
+		encoding::RiscVRegister upper_result = (upper_destination == src1 || upper_destination == src2)
+											   ? encoding::RiscVRegister::t4 : upper_destination;
+		if (has_upper_destination) {
+			batch += (is_signed ? encoding::MULH : encoding::MULHU)(upper_result, src1, src2);
+		}
+
+		batch += encoding::MUL(lower_destination, src1, src2);
+
+		if (upper_result != upper_destination) {
+			batch += encoding::MV(upper_destination, upper_result);
+		}
+	} else {
+		// in here we do not need MULH(U) because the result can be stored in 64 bit (hence MUL).
+
+		// move src1 and src2 into t1 and t2 respectively and prepare them for multiplication
+		if (is_signed) {
+			//TODO:
+		} else {
+			// IMUL3 loads the immediate directly, no need to load it
+			if (src1 != encoding::RiscVRegister::t1) {
+				batch += encoding::MV(encoding::RiscVRegister::t1, encoding::RiscVRegister::zero);
+				helper::move_to_register(batch, encoding::RiscVRegister::t1, src1, helper::operand_to_register_access(op_size), encoding::RiscVRegister::t4, true);
+			}
+			//TODO: load t2
+		}
+
+		//TODO: multiply
+
+		//TODO: store
+
+		//TODO: test
+	}
 }
 /*
 void CodeGenerator::translate_mul(const fadec::Instruction& inst, utils::riscv_instruction_t* riscv, size_t& count) {
