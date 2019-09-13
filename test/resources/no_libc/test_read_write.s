f2fc1e56dc4fbaca6dd851743bcaa075ee5b17c9
justuspolzin <justus.polzin@tum.de>
2019-09-13 20:21:13 +0200

    Change read_write integration test to not write memory below rsp

--- test/resources/no_libc/test_read_write.s
+++ test/resources/no_libc/test_read_write.s
@@ -9,50 +9,51 @@ _start:
 	xor rcx, rcx
 	mov rbx, 0x0001000100010001
 	xor rdx, rdx
-quad_jump:
-	mov [rsp + rcx*8 - 8], rdx
-	add rdx, rbx
-	dec rcx
-	cmp rcx, -0x400
-	jne quad_jump
 	sub rsp, 0x2000
+quad_jump:
+	mov [rsp + rcx*8], rdx
+	add rdx, rbx
+	inc rcx
+	cmp rcx, 0x400
+	jne quad_jump
 	
 	
 # write 0x400 doubles onto the stack
 	xor rcx, rcx
 	mov ebx, 0x00010001
 	xor edx, edx
-double_jump:
-	mov [rsp + rcx*4 - 4], edx
-	add edx, ebx
-	dec rcx
-	cmp rcx, -0x400
-	jne double_jump
 	sub rsp, 0x1000
+double_jump:
+	mov [rsp + rcx*4], edx
+	add edx, ebx
+	inc rcx
+	cmp rcx, 0x400
+	jne double_jump
 	
 	
 # write 0x400 words onto the stack
 	xor rcx, rcx
 	mov bx, 0x0001
 	xor dx, dx
-words_jump:
-	mov [rsp + rcx*2 - 2], dx
-	add dx, bx
-	dec rcx
-	cmp rcx, -0x400
-	jne words_jump
 	sub rsp, 0x800
+words_jump:
+	mov [rsp + rcx*2], dx
+	add dx, bx
+	inc rcx
+	cmp rcx, 0x400
+	jne words_jump
 	
 	
 # write 0x400 bytes onto the stack
 	xor rcx, rcx
 	mov bl, 0x01
 	xor dl, dl
+	sub rsp, 0x400
 byte_jump:
-	mov [rsp + rcx - 1], dl
+	mov [rsp + rcx], dl
 	add dl, bl
-	dec rcx
-	cmp rcx, -0x400
+	inc rcx
+	cmp rcx, 0x400
 	jne byte_jump
 
 
@@ -61,15 +62,15 @@ byte_jump:
 	mov rbx, 0x0001000100010001
 	xor rdx, rdx
 	xor rcx, rcx
+	sub rbp, 0x2000
 quad_verify:
-	mov rsi, [rbp + rcx*8 - 8]
+	mov rsi, [rbp + rcx*8]
 	cmp rsi, rdx
 	jne exit
 	add rdx, rbx
-	dec rcx
-	cmp rcx, -0x400
+	inc rcx
+	cmp rcx, 0x400
 	jne quad_verify
-	sub rbp, 0x2000
 	
 
 # verify the stack-content of the doubles
@@ -77,15 +78,15 @@ quad_verify:
 	mov ebx, 0x00010001
 	xor edx, edx
 	xor rcx, rcx
+	sub rbp, 0x1000
 double_verify:
-	mov esi, [rbp + rcx*4 - 4]
+	mov esi, [rbp + rcx*4]
 	cmp esi, edx
 	jne exit
 	add edx, ebx
-	dec rcx
-	cmp rcx, -0x400
+	inc rcx
+	cmp rcx, 0x400
 	jne double_verify
-	sub rbp, 0x1000
 
 
 # verify the stack-content of the words
@@ -93,15 +94,15 @@ double_verify:
 	mov bx, 0x0001
 	xor dx, dx
 	xor rcx, rcx
+	sub rbp, 0x800
 words_verify:
-	mov si, [rbp + rcx*2 - 2]
+	mov si, [rbp + rcx*2]
 	cmp si, dx
 	jne exit
 	add dx, bx
-	dec rcx
-	cmp rcx, -0x400
+	inc rcx
+	cmp rcx, 0x400
 	jne words_verify
-	sub rbp, 0x800
 
 
 # verify the stack-content of the bytes
@@ -109,19 +110,20 @@ words_verify:
 	mov bl, 0x01
 	xor dl, dl
 	xor rcx, rcx
+	sub rbp, 0x400
 byte_verify:
-	mov sil, [rbp + rcx - 1]
+	mov sil, [rbp + rcx]
 	cmp sil, dl
 	jne exit
 	add dl, bl
-	dec rcx
-	cmp rcx, -0x400
+	inc rcx
+	cmp rcx, 0x400
 	jne byte_verify
 	
 
+xor rdi, rdi
+
 # exit the program
 exit:
-	xor rdi, rdi
 	mov rax, 0x3c
 	syscall
-
